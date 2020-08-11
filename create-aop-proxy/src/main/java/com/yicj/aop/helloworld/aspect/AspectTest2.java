f993fa2b29281e98867c684909e9d85f609ec1ca
易成杰 <626659321@qq.com>
2020-08-11 19:41:37 +0800

    保存

---
+++ create-aop-proxy/src/main/java/com/yicj/aop/helloworld/aspect/AspectTest2.java
@@ -0,0 +1,29 @@
+package com.yicj.aop.helloworld.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+/**
+ * ClassName: AspectTest2
+ * Description: TODO(描述)
+ * Date: 2020/8/11 19:39
+ *
+ * @author yicj(626659321 @ qq.com)
+ * 修改记录
+ * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息
+ */
+@Slf4j
+@Aspect
+@Component
+public class AspectTest2 {
+    @Pointcut("execution(* com.yicj.aop..*.*(..))")
+    public void test2(){}
+
+    @Before("test2()")
+    public void beforeTest(){
+        log.info("before test2....");
+    }
+}
