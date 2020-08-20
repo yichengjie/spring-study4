e291b0278fe21f045863e516ae6a38bdbf925254
易成杰 <626659321@qq.com>
2020-08-20 09:15:00 +0800

    保存

--- best-practices-note/src/main/java/com/yicj/study/common/BaseDao.java
+++ best-practices-note/src/main/java/com/yicj/study/common/BaseDao.java
@@ -4,7 +4,7 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
 public class BaseDao<T> {
-    public Class clazz;
+    public Class<T> clazz;
 
     public BaseDao() {
         Type superclass = getClass().getGenericSuperclass();
